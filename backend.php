0df5a28db91fd4a046c77dcb34e16b87e6f77d6f
C-pato <chyannsean1@gmail.com>
2026-06-01 09:44:43 +0200

    Class added

--- backend.php
+++ backend.php
@@ -1,9 +1,13 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "sandbox1";
+
+class Database {  
+ public function connect() {
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "sandbox1";
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
-
+    }
+} 
 ?>
