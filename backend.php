6395236c880cc97d05f4e9304c8942e7a2bbe647
C-pato <chyannsean1@gmail.com>
2026-06-01 10:46:56 +0200

    Addedd validation for the connection

--- backend.php
+++ backend.php
@@ -9,5 +9,11 @@ class Database {
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
     }
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+    else if ($conn) {
+        echo "Connected successfully";
+    }
+
 } 
 ?>
