cc0054ad386d1f8bca9370e9e3ca225f8f70367f
C-pato <chyannsean1@gmail.com>
2026-06-15 13:43:07 +0200

    added studenten functiee select studenten

---
+++ Studenten.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Studenten
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+        $this->db->connect();
+    }
+    public function read()
+    {
+        $sql = "SELECT id, voornaam, achternaam FROM studenten";
+        $result = mysqli_query($this->db->conn, $sql);
+
+        return $result;
+    }
+}
+
+
+?>
