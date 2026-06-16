2e86eb3aa31941c7a8016351e94928bf03a1547c
C-pato <chyannsean1@gmail.com>
2026-06-16 13:42:11 +0200

    added all teacher functions everything works

--- teacher.php
+++ teacher.php
@@ -24,12 +24,12 @@ class Teachers
             die('Geen toegang');
         }
 
-        $sql = "SELECT id, naam, email, wachtwoord FROM users";
+        $sql = "SELECT id, naam, email, wachtwoord, role FROM users";
         return mysqli_query($this->db->conn, $sql);
     }
 
 
-
+    // TEACHER ADD
     public function add($naam, $email, $wachtwoord, $role)
     {
         if (session_status() == PHP_SESSION_NONE) {
@@ -57,6 +57,48 @@ class Teachers
         }
 
         return "Error: " . mysqli_error($this->db->conn);
+    }
+
+    public function delete($naam, $email)
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (
+            !isset($_SESSION['role']) ||
+            ($_SESSION['role'] != 'admin')
+        ) {
+            return "Geen toestemming om teachers te verwijderen.";
+        }
+        $sql = "DELETE FROM users WHERE naam = '$naam' AND email = '$email'";
+
+        if (mysqli_query($this->db->conn, $sql)) {
+            return "Record deleted successfully";
+        } else {
+            return "Error deleting record: " . mysqli_error($this->db->conn);
+        }
+    }
+    public function update($naam, $email, $wachtwoord, $role, $id)
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (
+            !isset($_SESSION['role']) ||
+            ($_SESSION['role'] != 'admin')
+        ) {
+            return "Geen toestemming om teachers te wijzigen.";
+        }
+
+        $sql = "UPDATE users SET naam = '$naam', email = '$email', wachtwoord = '$wachtwoord', role = '$role' WHERE id = '$id'";
+
+        if (mysqli_query($this->db->conn, $sql)) {
+            return "Record updated successfully";
+        } else {
+            return "Error updating record: " . mysqli_error($this->db->conn);
+        }
     }
 }
 ?>
